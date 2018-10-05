ef864fde94448bb296db1ff1edb65ecf63f07376
natcodes <natalienuno@gmail.com>
2018-10-05 11:25:11 -0700

    Initial Commit

---
+++ stack_asset_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stack_asset_test
+//
+//  Created by Natalie Nuno on 10/5/18.
+//  Copyright © 2018 Natalie Nuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ stack_asset_testTests/stack_asset_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  stack_asset_testTests.swift
+//  stack_asset_testTests
+//
+//  Created by Natalie Nuno on 10/5/18.
+//  Copyright © 2018 Natalie Nuno. All rights reserved.
+//
+
+import XCTest
+@testable import stack_asset_test
+
+class stack_asset_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
